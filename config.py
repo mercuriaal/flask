4281e8506d4c7d05c6f1336ded4f3d5e82052dc6
Ilya Lazarev <ilya.v.lazarev@gmail.com>
2021-07-11 00:43:04 +0300

    db config

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
 import os
-import psycopg2
 
-DATABASE_URL = os.environ['DATABASE_URL']
+POSTGRE_URI = os.getenv('DATABASE', 'Здесь переменная окружения')
 SALT = 'gc48956347g'
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
