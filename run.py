b8c1abf1a0fef4cef5a68be3899950ef0f2aff97
Ilya Lazarev <ilya.v.lazarev@gmail.com>
2021-07-27 10:44:51 +0300

    routes for email sender added

--- run.py
+++ run.py
@@ -1,12 +1,16 @@
 import os
 
 from app import app
-from views import AdvertisementView
+from views import AdvertisementView, EmailSender
 
 app.add_url_rule('/advertisements/', view_func=AdvertisementView.as_view('ads_list'),
                  methods=['GET', 'POST'])
 app.add_url_rule('/advertisements/<int:ad_id>', view_func=AdvertisementView.as_view('ads_retrieve'),
                  methods=['GET', 'PATCH', 'DELETE'])
+app.add_url_rule('/email', view_func=EmailSender.as_view('post_task'),
+                 methods=['POST'])
+app.add_url_rule('/email/<string:task_id>', view_func=EmailSender.as_view('get_task'),
+                 methods=['GET'])
 
 if __name__ == '__main__':
     app.run(
